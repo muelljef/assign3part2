1552ba5aa5dd94f252c9ee835483f1319915e14d
muelljef <muelljef@onid.oregonstate.edu>
2015-04-22 10:03:19 +1000

    Got AJAX request for gists working and saved to an object that I can access, played with editing html onload and with a button in Javascript functions

--- scripts.js
+++ scripts.js
@@ -1,1 +1,9 @@
-var hello = 'hello';
+//instance of http request
+var httpRequest = new XMLHttpRequest();
+
+/*httpRequest.onreadystatechange = function(){
+  process the server response
+};*/
+
+httpRequest.open('GET', //github.com/gists/public, true);
+httRequest.send(null);
